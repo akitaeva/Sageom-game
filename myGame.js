7c1ef2d17587b19c2eb9ddb042eb7bcf2dedf2f0
Alina Kitaeva <akitaeva@gmail.com>
2018-06-27 14:20:16 -0400

    position indexes added

--- myGame.js
+++ myGame.js
@@ -1,51 +1,54 @@
 var puzzlePieces = [
-    {img: 'seedOfLife01.png'},
-    {img: 'seedOfLife02.png'},
-    {img: 'seedOfLife03.png'}, 
-    {img: 'seedOfLife04.png'}, 
-    {img: 'seedOfLife05.png'}, 
-    {img: 'seedOfLife06.png'}, 
-    {img: 'seedOfLife07.png'}, 
-    {img: 'seedOfLife08.png'}, 
-    {img: 'seedOfLife09.png'}, 
+    {name: '1', img: 'seedOfLife01.png', positionIndx: 0, rotationValue: 0},
+    {name: '2', img: 'seedOfLife02.png', positionIndx: 1, rotationValue: 0},
+    {name: '3', img: 'seedOfLife03.png', positionIndx: 2, rotationValue: 0}, 
+    {name: '4', img: 'seedOfLife04.png', positionIndx: 3, rotationValue: 0}, 
+    {name: '5', img: 'seedOfLife05.png', positionIndx: 4, rotationValue: 0}, 
+    {name: '6', img: 'seedOfLife06.png', positionIndx: 5, rotationValue: 0}, 
+    {name: '7', img: 'seedOfLife07.png', positionIndx: 6, rotationValue: 0}, 
+    {name: '8', img: 'seedOfLife08.png', positionIndx: 7, rotationValue: 0}, 
+    {name: '9', img: 'seedOfLife09.png', positionIndx: 8, rotationValue: 0}, 
 
-]
-// var myCanvas = document.getElementById('theCanvas');
-// var ctx = myCanvas.getContext('2d');
-// console.log(myCanvas)
-// var myGame = new Sageom(puzzlePieces);
+];
+
 var myGame;
-// myGame.shuffleBoard();
-
 
 $(document).ready(function(){
+    
 
+var Sageom = function(array) {
+        this.pieces = array; //getting access to the array
+        var pickedPieces = []; // comparison array
+        var totalClicks = 0;
+        this.shuffleBoard(this.pieces); // calling the shuffle function
+        //assinging correct value for position index of each element
+        console.log("pieces before: ", this.pieces);
+        
 
+        this.pieces.forEach(function(element, i){
+            console.log(element);
+            $(element).data("positionIndex", i);
+            // var that = this;
+            // element.setAttribute("name", i);
+            console.log(element);
+        });
+        console.log(" pieces after: ", this.pieces);
+        //displaying each individual picture in the dom
+        var html = "";
+        this.pieces.forEach(function(pic, index) {
+          html += '<div class= "pieces" name ="' + pic.positionIndx + '" id="piece_' + pic.name + '"' ;
+          html += ' style="background: url(./images/' + pic.img + '") no-repeat"> ';
+          html += "</div>";
+          $('#playBoard').html(html);
+        }); 
+        document.getElementById("playBoard").innerHTML = html;
+    }; //end of Sageom
 
-
-var Sageom  = function (array) {
-    this.pieces = array;
-
-  }; //end of Sageom
-
-// calling sageom cf
-  Sageom(puzzlePieces)
-
-var placePuzzlePieces = function() {
-    this.pieces.forEach(function (){
-
-    }
-
-
-)
-
-}; //end of placePuzzlePieces
 
 Sageom.prototype.shuffleBoard = function () {
   
     var newBoard =  this.pieces;
   
-
     for (var i = this.pieces.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = newBoard[i];
@@ -54,22 +57,58 @@ Sageom.prototype.shuffleBoard = function () {
     }
     
     this.pieces = newBoard;
-    console.log(this.pieces)
-
+    console.log(this.pieces);
 
 }; //end of shuffleBoard
 
 
+Sageom.prototype.checkElement = function (firstClick, secondClick) {
+    if(firstClick.attr('name') === (secondClick.attr('name'))) {
+      return true;
+    }
+    return false;
+   }
+
+
+
+
 document.getElementById("start-button").onclick = function() {
     myGame = new Sageom(puzzlePieces);
-    myGame.shuffleBoard(puzzlePieces);
-    
- 
-    
   };
 
 
-});
+  $('.pieces').on('click', function() {
+    totalClicks++;
+    var elmntIndex;
+
+    myGame.pickedPieces.push($(this));
+
+    
+    // if ( myGame.pickedPieces.length === 2) {
+    //     var result = myGame.checkElement(myGame.pickedPieces[0], myGame.pickedPieces[1])
+    //     if (!result) {
+    //         setTimeout(function() {
+    //             //switch position indexes of the elements
+
+    //             myGame.pickedPieces = [];
+    //         }, 1000);
+    //     }
+    //     else {
+    //         myGame.pickedPieces = [];
+
+    //     }
+   
+
+    // }
+
+
+
+
+  }); //end of on-click pieces
+
+
+
+}); //end of doc. ready
 
 
 

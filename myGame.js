7a9859f6cfe7b468e1d5fbb70d42ffaaa33377a4
Alina Kitaeva <akitaeva@gmail.com>
2018-06-28 02:08:51 -0400

    changes for the day

--- myGame.js
+++ myGame.js
@@ -1,18 +1,18 @@
 var puzzlePieces = [
-    {name: '1', img: 'seedOfLife01.png', positionIndx: 0, rotationValue: 0},
-    {name: '2', img: 'seedOfLife02.png', positionIndx: 1, rotationValue: 0},
-    {name: '3', img: 'seedOfLife03.png', positionIndx: 2, rotationValue: 0}, 
-    {name: '4', img: 'seedOfLife04.png', positionIndx: 3, rotationValue: 0}, 
-    {name: '5', img: 'seedOfLife05.png', positionIndx: 4, rotationValue: 0}, 
-    {name: '6', img: 'seedOfLife06.png', positionIndx: 5, rotationValue: 0}, 
-    {name: '7', img: 'seedOfLife07.png', positionIndx: 6, rotationValue: 0}, 
-    {name: '8', img: 'seedOfLife08.png', positionIndx: 7, rotationValue: 0}, 
-    {name: '9', img: 'seedOfLife09.png', positionIndx: 8, rotationValue: 0}, 
+    {name: '0', img: 'seedOfLife01.png', positionIndx: 0, rotationValue: 0},
+    {name: '1', img: 'seedOfLife02.png', positionIndx: 1, rotationValue: 0},
+    {name: '2', img: 'seedOfLife03.png', positionIndx: 2, rotationValue: 0}, 
+    {name: '3', img: 'seedOfLife04.png', positionIndx: 3, rotationValue: 0}, 
+    {name: '4', img: 'seedOfLife05.png', positionIndx: 4, rotationValue: 0}, 
+    {name: '5', img: 'seedOfLife06.png', positionIndx: 5, rotationValue: 0}, 
+    {name: '6', img: 'seedOfLife07.png', positionIndx: 6, rotationValue: 0}, 
+    {name: '7', img: 'seedOfLife08.png', positionIndx: 7, rotationValue: 0}, 
+    {name: '8', img: 'seedOfLife09.png', positionIndx: 8, rotationValue: 0}, 
 
 ];
 
 var myGame;
-
+console.log(puzzlePieces)
 $(document).ready(function(){
     
 
@@ -22,21 +22,26 @@ var Sageom = function(array) {
         var totalClicks = 0;
         this.shuffleBoard(this.pieces); // calling the shuffle function
         //assinging correct value for position index of each element
-        console.log("pieces before: ", this.pieces);
+        // console.log("pieces before: ", this.pieces);
+
         
 
         this.pieces.forEach(function(element, i){
-            console.log(element);
-            $(element).data("positionIndex", i);
+            // 
+            // console.log('this is a lement' + element,i);
+            // html +=
+            // $(element).data("positionIndex", i);
             // var that = this;
-            // element.setAttribute("name", i);
-            console.log(element);
+            // $(element).setAttribute("name", i);
+            // console.log( 'after shuffle'+element);
         });
-        console.log(" pieces after: ", this.pieces);
+        console.log(this.pieces);
         //displaying each individual picture in the dom
         var html = "";
+        var that = this;
         this.pieces.forEach(function(pic, index) {
-          html += '<div class= "pieces" name ="' + pic.positionIndx + '" id="piece_' + pic.name + '"' ;
+        var index = that.pieces.indexOf(pic);
+          html += '<div class= "pieces" name ="' + pic.name + '" id="' + index + '"' ;
           html += ' style="background: url(./images/' + pic.img + '") no-repeat"> ';
           html += "</div>";
           $('#playBoard').html(html);
@@ -74,39 +79,32 @@ Sageom.prototype.checkElement = function (firstClick, secondClick) {
 
 document.getElementById("start-button").onclick = function() {
     myGame = new Sageom(puzzlePieces);
-  };
-
-
-  $('.pieces').on('click', function() {
-    totalClicks++;
-    var elmntIndex;
-
-    myGame.pickedPieces.push($(this));
-
+    $('.pieces').on('click', function() {
+        console.log(this);
+        var domPositions = [];
+        
+        // totalClicks++;
+        // var elmntIndex;
     
-    // if ( myGame.pickedPieces.length === 2) {
-    //     var result = myGame.checkElement(myGame.pickedPieces[0], myGame.pickedPieces[1])
-    //     if (!result) {
-    //         setTimeout(function() {
-    //             //switch position indexes of the elements
-
-    //             myGame.pickedPieces = [];
-    //         }, 1000);
-    //     }
-    //     else {
-    //         myGame.pickedPieces = [];
-
-    //     }
-   
-
-    // }
-
-
-
-
-  }); //end of on-click pieces
-
-
+        // myGame.pickedPieces.push($(this));
+    
+        
+        // if ( myGame.pickedPieces.length === 2) {
+        //     var result = myGame.checkElement(myGame.pickedPieces[0], myGame.pickedPieces[1])
+        //     if (!result) {
+        //         setTimeout(function() {
+        //             //switch the position indexes of the elements
+    
+        //             myGame.pickedPieces = [];
+        //         }, 1000);
+        //     }
+        //     else {
+        //         myGame.pickedPieces = [];
+    
+        //     }
+        // }
+      }); //end of on-click pieces
+  };
 
 }); //end of doc. ready
 
